b2fa4eac3fcdacc4d09aad0e0c8df98e9fce058b
vaishnavrajesh <RajeshV@ho.FosLtd.co.za>
2017-07-17 11:45:05 +0200

    Included changes to read jar files from cordys location and push it into a local file storage

--- src/za/co/tfg/arts/controller/CONSTANTS.java
+++ src/za/co/tfg/arts/controller/CONSTANTS.java
@@ -4,4 +4,6 @@ public class CONSTANTS {
 	public static final String ZIP_FILE_LOCATION = "C:/Users/rajeshv/Downloads/zip";
 	public static final String UNZIP_FILE_LOCATION = "C:/Users/rajeshv/Downloads/unzip";
 	public static final String DB_SQL_OUTPUT_FILE_LOCATION = "C:/Users/rajeshv/Desktop/SQL";
+	public static final String LFS_JAR_FILE_LOCATION = "\\\\uat-cordys03\\E$\\Cordys\\CU1\\com";
+	public static final String OUTPUT_JAR_FILE_LOCATION = "C:/Users/rajeshv/Downloads/jar";
 }
--- src/za/co/tfg/arts/controller/DirectoryIterator.java
+++ src/za/co/tfg/arts/controller/DirectoryIterator.java
@@ -1,18 +1,27 @@
 package za.co.tfg.arts.controller;
 
 import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
 import java.io.IOException;
+import java.io.InputStream;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.util.Enumeration;
+import java.util.jar.JarEntry;
+import java.util.jar.JarFile;
+import java.util.jar.JarOutputStream;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class DirectoryIterator {
 
 	public static void main(String[] args) throws IOException {
-		new DirectoryIterator().start();
+		new DirectoryIterator().traverseCordysFiles(CONSTANTS.LFS_JAR_FILE_LOCATION);
 	}
 
-	private void start() throws IOException{
+	private void initiateExtractionProcess() throws IOException{
 		traverseZIPFileDirectory(CONSTANTS.ZIP_FILE_LOCATION);
 
 		File [] files = getFileList(new File(CONSTANTS.UNZIP_FILE_LOCATION));
@@ -53,7 +62,70 @@ public class DirectoryIterator {
 			}
 		}
 	}
+	
+	
+	public static void copyJarFile(JarFile jarFile, File destDir) throws IOException {
+	       String fileName = jarFile.getName();
+	       String fileNameLastPart = fileName.substring(fileName.lastIndexOf(File.separator));
+	       File destFile = new File(destDir, fileNameLastPart);
 
+	       JarOutputStream jos = new JarOutputStream(new FileOutputStream(destFile));
+	       Enumeration<JarEntry> entries = jarFile.entries();
 
+	       while (entries.hasMoreElements()) {
+	           JarEntry entry = entries.nextElement();
+	           InputStream is = jarFile.getInputStream(entry);
 
+	           //jos.putNextEntry(entry);
+	           //create a new entry to avoid ZipException: invalid entry compressed size
+	           jos.putNextEntry(new JarEntry(entry.getName()));
+	           byte[] buffer = new byte[4096];
+	           int bytesRead = 0;
+	           while ((bytesRead = is.read(buffer)) != -1) {
+	               jos.write(buffer, 0, bytesRead);
+	           }
+	           is.close();
+	           jos.flush();
+	           jos.closeEntry();
+	       }
+	       jos.close();
+	   }
+	
+	
+	private boolean isTFGJarFile(String jarFileName){
+
+		    Pattern pattern = Pattern.compile("ROP|tfg|Retail",Pattern.CASE_INSENSITIVE);
+		    Matcher m = pattern.matcher(jarFileName);
+		    if(m.find()){
+		    	return true;
+		    }
+		    return false;
+		
+	}
+	
+	private void traverseCordysFiles(String directoryPath) throws FileNotFoundException, IOException{
+		File [] files = getFileList(new File(directoryPath));
+		for (int i = 0; i < files.length; i++){
+			String jartFileName = files[i].getName();
+			if (files[i].isFile() && jartFileName.indexOf(".jar")>-1 && isTFGJarFile(jartFileName)){				  
+				
+				String outputFilePath = CONSTANTS.OUTPUT_JAR_FILE_LOCATION+File.separator+files[i].getName();
+				File file = new File(outputFilePath);
+				
+				file.getParentFile().mkdirs();
+				/*if(file.exists()){
+					file.delete();	
+				}
+				file.createNewFile();
+				*/
+				copyJarFile(new JarFile(files[i]), new File(CONSTANTS.OUTPUT_JAR_FILE_LOCATION));
+			
+			}else{
+				if(files[i].isDirectory()){
+					traverseCordysFiles(files[i].getAbsolutePath());
+				}
+			}
+		}
+	}
+	
 }
